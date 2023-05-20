ce1c663b8b52ba0fc81e68667c3b2882083ebec7
KeaweAquarian <keaweaquarian@gmail.com>
2023-05-19 20:04:27 -0400

    Initial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,8 @@
-//Author Keawe Aquarian
+/**
+ * @author Keawe Aquarian
+ * @version 1.0
+ * @since 01/01/2023
+ */
 import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,8 @@
+/**
+ * @author Keawe Aquarian
+ * @version 1.0
+ * @since 01/01/2023
+ */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
